85b884a0728ebc251ba7f6ca2b1b0ba05fc904ae
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2020-02-02 17:00:38 -0800

    add db query

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,18 +1,45 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-const Mailer = require('../services/Mailer');
+const _ = require('lodash')
+const { Path } = require('path-parser')
+const { URL } = require('url')
+const mongoose = require('mongoose')
+const requireLogin = require('../middlewares/requireLogin')
+const requireCredits = require('../middlewares/requireCredits')
+const Mailer = require('../services/Mailer')
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
-
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting')
     })
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        console.log(req.body)
+        const parser = new Path('/api/surveys/:surveyId/:choice')
+
+        // Checks for duplicate clicks (yes/no)
+        _.chain(req.body)
+            .map(({ email, url }) => {
+                const match = parser.test(new URL(url).pathname)
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice }
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                   _id: surveyId,
+                   recipients: {
+                       $elemMatch: { email: email, responded: false }
+                   } 
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec()
+            })
+            .value()
+
         res.send({})
     })
 
@@ -26,7 +53,7 @@ module.exports = app => {
           recipients: recipients.split(',').map(email => ({ email: email.trim() })),
           _user: req.user.id,
           dateSent: Date.now()
-        });
+        })
     
         // Send email
         const mailer = new Mailer(survey, surveyTemplate(survey));
@@ -41,5 +68,5 @@ module.exports = app => {
             res.status(422).send(err)
         }
         
-      });
+      })
 }
